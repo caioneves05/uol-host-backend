ef9ffe232897ce00d3d10cbab5c34f6cd5fef790
caioneves05 <ncaio037@gmail.com>
2025-06-26 22:02:06 -0300

    feat: initial structure

---
+++ src/main/java/uol_host_backend/demo/UolChallengeBackendApplication.java
@@ -0,0 +1,13 @@
+package uol_host_backend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UolChallengeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UolChallengeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uol_host_backend/demo/UolChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uol_host_backend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UolChallengeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
