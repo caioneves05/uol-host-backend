f73a8790c95c6edb9dc54c0dda7643f47810a49a
caioneves05 <ncaio037@gmail.com>
2026-02-05 21:25:49 -0300

    feat: aws s3 bucket versioning

--- iac/main.tf
+++ iac/main.tf
@@ -11,3 +11,26 @@ provider "aws" {
   region  = "us-east-2"
   profile = "caioeniac"
 }
+
+resource "aws_s3_bucket" "terraform_state" {
+    bucket = "uol-host-terraform-state"
+    acl    = "private"
+    force_destroy = true
+
+    lifecycle {
+      prevent_destroy = true
+    }
+
+    tags = {
+        iac         = "true"
+        profile     = "caioeniac"
+        environment = "dev"
+    }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
